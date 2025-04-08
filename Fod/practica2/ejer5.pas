789c88922b64b53904aa80ae5e0ba8aba33d1ffb
valentino <valenkvolek@gmail.com>
2025-04-08 19:25:33 -0300

    ...

--- Fod/practica2/ejer5.pas
+++ Fod/practica2/ejer5.pas
@@ -1,18 +1,4 @@
-(*Suponga que trabaja en una oficina donde está montada una LAN (red local). La misma fue
-construida sobre una topología de red que conecta 5 máquinas entre sí y todas las
-máquinas se conectan con un servidor central. Semanalmente cada máquina genera un
-archivo de logs informando las sesiones abiertas por cada usuario en cada terminal y por
-cuánto tiempo estuvo abierta. Cada archivo detalle contiene los siguientes campos:
-cod_usuario, fecha, tiempo_sesion. Debe realizar un procedimiento que reciba los archivos
-detalle y genere un archivo maestro con los siguientes datos: cod_usuario, fecha,
-tiempo_total_de_sesiones_abiertas.
-Notas:
-● Cada archivo detalle está ordenado por cod_usuario y fecha.
-● Un usuario puede iniciar más de una sesión el mismo día en la misma máquina, o
-inclusive, en diferentes máquinas.
-● El archivo maestro debe crearse en la siguiente ubicación física: /var/log.*) 
-
-program ejer5prac2
+program ejer5prac2;
 
 uses
     SysUtils;
@@ -24,13 +10,13 @@ const
 type
     detalle = record
         cod_usuario: integer;
-        fecha: string;
+        fecha: string[10];  
         tiempo_sesion: real;
     end;
 
     maestro = record
         cod_usuario: integer;
-        fecha: string;
+        fecha: string[10];  
         tiempo_total_de_sesiones_abiertas: real;
     end;
 
@@ -44,22 +30,21 @@ begin
     if not eof(arch) then
         read(arch, dato)
     else
-        dato.cod_usuario := VALOR_ALTO; 
+        dato.cod_usuario := VALOR_ALTO;
 end;
 
 procedure minimo(var dets: detalles; var regs: regDetalles; var min: detalle);
 var
     i, posMin: integer;
 begin
-    min.cod_usuario := VALOR_ALTO;  
+    min.cod_usuario := VALOR_ALTO;
     posMin := 1;
-    for i := 1 to CANT_MAQUINAS do begin  
+    for i := 1 to CANT_MAQUINAS do
         if (regs[i].cod_usuario < min.cod_usuario) or 
            ((regs[i].cod_usuario = min.cod_usuario) and (regs[i].fecha < min.fecha)) then begin
             min := regs[i];
             posMin := i;
         end;
-    end;
     leer(dets[posMin], regs[posMin]);
 end;
 
@@ -71,18 +56,25 @@ var
     regm: maestro;
     min: detalle;
     actual_cod: integer;
-    actual_fecha: string;
-    total: real;  
+    actual_fecha: string[10];
+    total: real;
 begin
-    assign(mae, '/var/log/maestro');  
-    rewrite(mae);  
+    assign(mae, 'maestro');  
+    rewrite(mae);
+
     for i := 1 to CANT_MAQUINAS do begin
-        assign(vd[i], 'detalle' + IntToStr(i));  
-        reset(vd[i]);  
-        leer(vd[i], vrd[i]); 
+        assign(vd[i], 'detalle' + IntToStr(i));
+        if FileExists('detalle' + IntToStr(i)) then begin
+            reset(vd[i]);
+            leer(vd[i], vrd[i]);
+            writeln('detalle', i, ': cod=', vrd[i].cod_usuario, 
+                    ', fecha=', vrd[i].fecha, ', tiempo=', vrd[i].tiempo_sesion:0:2);
+        end else
+            writeln('Error: detalle', i, ' no existe');
     end;
 
     minimo(vd, vrd, min);
+
     while min.cod_usuario <> VALOR_ALTO do begin
         actual_cod := min.cod_usuario;
         actual_fecha := min.fecha;
@@ -95,11 +87,23 @@ begin
         regm.fecha := actual_fecha;
         regm.tiempo_total_de_sesiones_abiertas := total;
         write(mae, regm);
+
     end;
 
-    close(mae);  
+    close(mae);
+
     for i := 1 to CANT_MAQUINAS do
-        close(vd[i]);  
+        if FileExists('detalle' + IntToStr(i)) then
+            close(vd[i]);
+
+
+    reset(mae);
+    while not eof(mae) do begin
+        read(mae, regm);
+        writeln('usuario: ', regm.cod_usuario, ' fecha: ', regm.fecha, 
+                ' total horas: ', regm.tiempo_total_de_sesiones_abiertas:0:2);
+    end;
+    close(mae);
 end;
 
 var
